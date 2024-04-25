85a4adec533257c59541847550c4bcb50e290b45
alexshah1 <alexandershahramanian@gmail.com>
2024-04-26 00:43:28 +0400

    pypi description

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, find_packages
+import os
+
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open('pypi_desc.md', encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
 
 # Add install requirements
 setup(
@@ -6,7 +15,7 @@ setup(
     description="A package for book recommendation.",
     name="kitab",
     packages=find_packages(include=["kitab", "kitab.*"]),
-    version="0.0.18",
+    version="0.0.19",
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -14,4 +23,6 @@ setup(
     ],
     install_requires=['pandas', 'sentence-transformers>=2.6', 'psycopg2-binary', 'pgvector', 'python-dotenv', 'tqdm'],
     python_requires=">=3.9",
+    long_description=long_description,
+    description_content_type='text/markdown',
 )
