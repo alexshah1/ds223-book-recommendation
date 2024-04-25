fa20bd7852e72a0ed9f23d55c1a0c59ad42f49b2
alexshah1 <alexandershahramanian@gmail.com>
2024-04-25 10:27:33 +0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 # Add install requirements
 setup(
-    author="Alexander Shahramanyn",
+    author="Alexander Shahramanyan",
     description="A package for book recommendation.",
     name="kitab",
     packages=find_packages(include=["kitab", "kitab.*"]),
-    version="0.0.1",
+    version="0.0.2",
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    # install_requires=['numpy>=1.10', 'pandas'],
-    python_requires=">=3.7",
+    install_requires=['pandas', 'sentence-transformers>=2.6', 'psycopg2', 'pgvector', 'logging'],
+    python_requires=">=3.10",
 )
